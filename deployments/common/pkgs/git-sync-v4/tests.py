49fa7005c510f191a3570534a3d70d6abc16735e
Todd Miller <jmiller@stsci.edu>
2023-12-02 17:32:50 -0500

    Work on git-sync-4

--- deployments/common/pkgs/git-sync-v4/tests.py
+++ deployments/common/pkgs/git-sync-v4/tests.py
@@ -1,3 +1,22 @@
 import pytest
 
+INIT_SCRIPT = """
+set -eux
 
+chmod -R 777 test-clone || true
+rm -rf test-clone
+git clone  https://github.com/spacetelescope/nersc-refresh-announcements  test-clone
+cd test-clone
+echo "Made up user file." >made-up-file.ipynb
+echo "Add some stuff to modify" >>README.md
+mkdir -p new-dir/sub-dir
+echo "Made up user file." >new-dir/made-up-2.txt
+echo "Made up user file." >new-dir/sub-dir/made-up-3.txt
+rm -f LICENSE
+echo "Poison!" >poison-file.txt
+chmod 000 poison-file.txt
+mkdir -p poison-dir poison-dir-2
+echo "Poison!" >poison-dir/poison-file-2.txt
+chmod 644 poison-dir/poison-file-2.txt
+chmod 000 poison-dir poison-dir-2
+"""
